daa6046b15f5e00a82632fbc090f7a0ea7c61642
Fellipe de Oliveira <fellipe.oli.p@gmail.com>
2025-01-13 19:36:09 -0300

    Adicionando estrutura de dados array

---
+++ Array/Array.js
@@ -0,0 +1,13 @@
+// Array, como já bastante comum nas linguagens de programção, é cuma coleção de elementos ordenadas, começando pelo índice 0 e por eles, podem ser acessados. É utilizado para armazenar lista de valores em sua maioria.
+// Suas principais características são: Ordenado, permite duplicatas, elementos acessados por índices numéricos e aceitam qualquer tipo de dado, incluindo objetos e funções.
+
+// Aqui seguimos para os exemplos de todos os métodos utilizados na estrutura de métodos em array:
+
+1. //Adicionar elementos
+const frutas = ['maça', 'banana'];
+frutas.push('goiaba'); //add no final
+frutas.unshift('mamão'); // add no começo
+console.log(frutas);
+
+2, //Remover elementos
+
