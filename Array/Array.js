8590e5faf27f8fa9f16163d012911e6069b60ba8
Fellipe de Oliveira <fellipe.oli.p@gmail.com>
2025-01-14 22:41:52 -0300

    atualizando métodos

--- Array/Array.js
+++ Array/Array.js
@@ -14,3 +14,12 @@ const ultimaFruta = frutas.pop();// remove a ultima
 const primeiraFruta = frutas.shift(); //remove a primeira
 console.log(`${ultimaFruta} - ${primeiraFruta} - ${frutas}`);
 
+3. //Iteração
+//forEach - Para cada elemento(fruta) no array.
+frutas.forEach((fruta) => console.log(fruta)); // aparecerá a sequência das frutas.
+//map - usado para extrair uma parte do array e manipula-lo.
+const frutasMaiuscula = frutas.map(fruta => fruta.toUpperCase());
+console.log(frutasMaiuscula); // todas as frutas estarão em capslock
+
+4. // Para buscar elementos
+
